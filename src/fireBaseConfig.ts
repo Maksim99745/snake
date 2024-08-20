12451ef578fadcd06a717ab64f9d44fe028a2c01
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-08-21 00:37:04 +0300

    fix: correct build

--- src/fireBaseConfig.ts
+++ src/fireBaseConfig.ts
@@ -1,4 +1,3 @@
-import { getAnalytics } from 'firebase/analytics';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
@@ -13,5 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export default app;
